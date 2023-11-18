9d165e59bc26db54df814132bec19cbd987bfb68
Jay Pinion <jaypinion97@gmail.com>
2023-11-18 15:26:25 +0000

    fixed hover state of checkbox

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -9,26 +9,36 @@ import { cn } from "@/lib/utils";
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <div className="relative flex justify-center items-center">
-    <CheckboxPrimitive.Root
-      ref={ref}
-      className={cn(
-        "peer h-4 w-4 shrink-0 rounded-[2px] border-2 border-gs-black  disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gs-green data-[state=checked]:border-none data-[state=checked]:text-gs-white",
-        className
-      )}
-      {...props}
-    >
-      <CheckboxPrimitive.Indicator
-        className={cn("flex items-center justify-center text-current")}
+>(({ className, ...props }, ref) => {
+  const [isHovering, setIsHovering] = React.useState(false);
+  return (
+    <div className="relative flex justify-center items-center">
+      <CheckboxPrimitive.Root
+        ref={ref}
+        className={cn(
+          "peer h-4 w-4 shrink-0 rounded-[2px] border-2 border-gs-black  disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gs-green data-[state=checked]:border-none data-[state=checked]:text-gs-white",
+          className
+        )}
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+        {...props}
       >
-        <Check className="h-4 w-4" />
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
-    {/* // add a translucent gray circle over the checkbox on hover */}
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-8 h-8 flex items-center justify-center pointer-events-none opacity-0 group-hover:opacity-50 rounded-full bg-gs-lightGreen"></div>
-  </div>
-));
+        <CheckboxPrimitive.Indicator
+          className={cn("flex items-center justify-center text-current")}
+        >
+          <Check className="h-4 w-4" />
+        </CheckboxPrimitive.Indicator>
+      </CheckboxPrimitive.Root>
+      {/* // add a translucent gray circle over the checkbox on hover */}
+      <div
+        className={cn(
+          "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-8 h-8 flex items-center justify-center pointer-events-none rounded-full bg-gs-lightGreen",
+          isHovering ? "opacity-50" : "opacity-0"
+        )}
+      ></div>
+    </div>
+  );
+});
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
 export { Checkbox };
